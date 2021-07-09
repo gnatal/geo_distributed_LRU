1e0624800a18fa92eb943102c50af10b9c028fe0
gnatal <guilhermenatal47@gmail.com>
2021-07-08 23:04:06 -0300

    Described all cases

--- src/__tests__/hello.test.ts
+++ src/__tests__/hello.test.ts
@@ -2,9 +2,39 @@ import request from 'supertest'
 import server from '../app'
 
 describe('loading express', function () {
-  it('should create a new post', async () => {
+  it('should get the hello word', async () => {
     const res = await request(server).get('/')
 
     expect(res.statusCode).toEqual(200)
+  })
+
+  it('should get a cached resource', async () => {
+    const response = await request(server).get('/getResource')
+
+    expect(response.body).toBe({ data: 'true', cached: true })
+  })
+
+  it('should get a non cached resource', async () => {
+    const response = await request(server).get('/getResource')
+
+    expect(response.body).toBe({ data: 'true', cached: false })
+  })
+
+  it('should create a new resource', async () => {
+    const response = await request(server).get('/getResource')
+
+    expect(response.body).toBe({ data: 'true', cached: false })
+  })
+
+  it('should update a resource', async () => {
+    const response = await request(server).get('/getResource')
+
+    expect(response.body).toBe({ data: 'true', cached: false })
+  })
+
+  it('should delete a resource', async () => {
+    const response = await request(server).get('/getResource')
+
+    expect(response.body).toBe({ data: 'true', cached: false })
   })
 })
